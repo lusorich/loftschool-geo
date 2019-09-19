a02eb27a9f56e10544fd96cdfd15dbcc4d2f9124
Lusorich <dreamonautre@gmail.com>
2019-09-20 00:01:45 +0500

    listener click

--- Source/script.js
+++ Source/script.js
@@ -5,13 +5,13 @@ ymaps.ready(init);
 
 let array = [];
 let obj = {
-        name: '',
-        place: '',
-        comment: '',
-    };
+    name: '',
+    place: '',
+    comment: '',
+};
 
 
-function init () {
+function init() {
     // Создание экземпляра карты и его привязка к контейнеру с
     // заданным id ("map").
 
@@ -28,7 +28,7 @@ function init () {
 
     function getAddress(coords) {
 
-        ymaps.geocode(coords).then(function (res) {
+        ymaps.geocode(coords).then(function(res) {
             var firstGeoObject = res.geoObjects.get(0);
 
             placemark.properties
@@ -46,59 +46,53 @@ function init () {
         });
     }
 
-    myMap.events.add('click', function (e) {
+    myMap.events.add('click', function(e) {
 
         var coords = e.get('coords');
-            var MyBalloonContentLayoutClass = ymaps.templateLayoutFactory.createClass(
-                              '<label class="form__label" for="name"> <h2>Ваш отзыв</h2>' + 
-                              '<input class="form__input" type="text" name="name" id="name" type="text" value placeholder="Ваше имя" required=""></label>' + 
-                              '<label class="form__label" for="place"><input class="form__input" type="text" name="place" id="place" type="text" value placeholder="Укажите место" required=""></label>' +
-                              '<textarea name="comment" id="comment" cols="30" rows="10" placeholder="Поделитесь впечатлениями"></textarea>' +
-                              '<button class="button" id="button">Добавить</button>', {
-                                build: function () {
-                // Сначала вызываем метод build родительского класса.
-                MyBalloonContentLayoutClass.superclass.build.call(this);
-                // А затем выполняем дополнительные действия.
-        
-            }
-                              }
-        );
+
 
         getAddress(coords);
 
-        ymaps.geocode(coords).then(function (res) {
-        var firstGeoObject = res.geoObjects.get(0);
+        ymaps.geocode(coords).then(function(res) {
+            var firstGeoObject = res.geoObjects.get(0);
 
-         placemark.properties.set ({ balloonContentHeader: firstGeoObject.getAddressLine()})
-      })
+            placemark.properties.set({
+                balloonContentHeader: firstGeoObject.getAddressLine()
+            })
+        })
 
-        var placemark = new ymaps.Placemark(coords,{}, {balloonContentLayout: MyBalloonContentLayoutClass}, 
-        {
+        var placemark = new ymaps.Placemark(coords, {
+            balloonContentHeader: getAddress(coords),
+            balloonContentFooter: '<label class="form__label" for="name"> <h2>Ваш отзыв</h2>' +
+                '<input class="form__input" type="text" name="name" id="name" type="text" value placeholder="Ваше имя" required=""></label>' +
+                '<label class="form__label" for="place"><input class="form__input" type="text" name="place" id="place" type="text" value placeholder="Укажите место" required=""></label>' +
+                '<textarea name="comment" id="comment" cols="30" rows="10" placeholder="Поделитесь впечатлениями"></textarea>' +
+                '<button class="button" id="button">Добавить</button>'
+        }, {
             iconLayout: 'default#image',
             iconImageHref: 'placemark.svg',
             iconImageSize: [30, 40]
         });
 
+        var buttonAdd = document.getElementById('button');
+        var nameInput = document.querySelector('#name');
+        var placeInput = document.querySelector('#place');
+        var commentInput = document.querySelector('#comment');
+
+
+
         myMap.geoObjects.add(placemark);
         placemark.balloon.open();
+  
 
-                        let buttonAdd = document.querySelector('.button');
-let nameInput = document.querySelector('#name');
-let placeInput = document.querySelector('#place');
-let commentInput = document.querySelector('#comment');
-
-    if (myMap.balloon.isOpen()) {
-        console.log('5');
-        ymaps.domEvent.manager
-    .add(buttonAdd, 'click', function (event) {
-        console.log(event.get('type'));
     });
-    }
-        
-    });
-
-
-
-
-
 }
+
+
+document.addEventListener('click', (e) => {
+    elem = e.target;
+
+    if (elem.id == 'button') {
+        console.log('YES');
+    }
+})
